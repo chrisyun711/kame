9fad9539a260617bc1ebd193d444dec0ac86047c
chrisyun711 <deyamalei@gmail.com>
2014-03-27 15:46:29 +0800

    gai zuozhe daima

---
+++ js/html_actuator.js
@@ -0,0 +1,199 @@
+function HTMLActuator() {
+  this.tileContainer    = document.querySelector(".tile-container");
+  this.scoreContainer   = document.querySelector(".score-container");
+  this.bestContainer    = document.querySelector(".best-container");
+  this.messageContainer = document.querySelector(".game-message");
+  this.sharingContainer = document.querySelector(".score-sharing");
+
+  this.score = 0;
+}
+
+HTMLActuator.prototype.actuate = function (grid, metadata) {
+  var self = this;
+
+  window.requestAnimationFrame(function () {
+    self.clearContainer(self.tileContainer);
+
+    grid.cells.forEach(function (column) {
+      column.forEach(function (cell) {
+        if (cell) {
+          self.addTile(cell);
+        }
+      });
+    });
+
+    self.updateScore(metadata.score);
+    self.updateBestScore(metadata.bestScore);
+
+    if (metadata.terminated) {
+      if (metadata.over) {
+        self.message(false); // You lose
+      } else if (metadata.won) {
+        self.message(true); // You win!
+      }
+    }
+
+  });
+};
+
+// Continues the game (both restart and keep playing)
+HTMLActuator.prototype.continue = function () {
+  if (typeof ga !== "undefined") {
+    ga("send", "event", "game", "restart");
+  }
+
+  this.clearMessage();
+};
+
+HTMLActuator.prototype.clearContainer = function (container) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+};
+
+HTMLActuator.prototype.addTile = function (tile) {
+  var text = new Array();
+  text[1] = "龙少"
+  text[2] = "修几"
+  text[3] = "汪汪"
+  text[4] = "幼芽"
+  text[5] = "骆驼"
+  text[6] = "农民"
+  text[7] = "雫雫"
+  text[8] = "医生"
+  text[9] = "NAO"
+  text[10] = "大树"
+  text[11] = "贝姆"
+  text[12] = "龙老师"
+  text[13] = "咩咩"
+  text[14] = "42"
+  var self = this;
+  var text2 = function (n) { var r = 0; while (n > 1) r++, n >>= 1; return r; }
+
+  var wrapper   = document.createElement("div");
+  var inner     = document.createElement("div");
+  var position  = tile.previousPosition || { x: tile.x, y: tile.y };
+  var positionClass = this.positionClass(position);
+
+  // We can't use classlist because it somehow glitches when replacing classes
+  var classes = ["tile", "tile-" + tile.value, positionClass];
+
+  if (tile.value > 2048) classes.push("tile-super");
+
+  this.applyClasses(wrapper, classes);
+
+  inner.classList.add("tile-inner");
+  inner.textContent = text[text2(tile.value)];
+
+  if (tile.previousPosition) {
+    // Make sure that the tile gets rendered in the previous position first
+    window.requestAnimationFrame(function () {
+      classes[2] = self.positionClass({ x: tile.x, y: tile.y });
+      self.applyClasses(wrapper, classes); // Update the position
+    });
+  } else if (tile.mergedFrom) {
+    classes.push("tile-merged");
+    this.applyClasses(wrapper, classes);
+
+    // Render the tiles that merged
+    tile.mergedFrom.forEach(function (merged) {
+      self.addTile(merged);
+    });
+  } else {
+    classes.push("tile-new");
+    this.applyClasses(wrapper, classes);
+  }
+
+  // Add the inner part of the tile to the wrapper
+  wrapper.appendChild(inner);
+
+  // Put the tile on the board
+  this.tileContainer.appendChild(wrapper);
+};
+
+HTMLActuator.prototype.applyClasses = function (element, classes) {
+  element.setAttribute("class", classes.join(" "));
+};
+
+HTMLActuator.prototype.normalizePosition = function (position) {
+  return { x: position.x + 1, y: position.y + 1 };
+};
+
+HTMLActuator.prototype.positionClass = function (position) {
+  position = this.normalizePosition(position);
+  return "tile-position-" + position.x + "-" + position.y;
+};
+
+HTMLActuator.prototype.updateScore = function (score) {
+  this.clearContainer(this.scoreContainer);
+
+  var difference = score - this.score;
+  this.score = score;
+
+  this.scoreContainer.textContent = this.score;
+
+  if (difference > 0) {
+    var addition = document.createElement("div");
+    addition.classList.add("score-addition");
+    addition.textContent = "+" + difference;
+
+    this.scoreContainer.appendChild(addition);
+  }
+};
+
+HTMLActuator.prototype.updateBestScore = function (bestScore) {
+  this.bestContainer.textContent = bestScore;
+};
+
+HTMLActuator.prototype.message = function (won) {
+  var mytxt=new Array(11);  
+  mytxt[0]="应该没有人会死在这里吧";
+  mytxt[1]="应该没有人会死在这里吧";
+  mytxt[2]="骆驼的Dior摸不到叻！";
+  mytxt[3]="没有肉包子不开心";
+  mytxt[4]="红酒配チョコ一口胖一斤";
+  mytxt[5]="医生的功能磁共振排队哈长呀！";
+  mytxt[6]="あ、俺俺、だから俺だよ";
+  mytxt[7]="削除掉NAO小可爱才有树哥";
+  mytxt[8]="等不到情深深雨蒙蒙";
+  mytxt[9]="窥伐到龙老师细哈耶则";
+  mytxt[10]="龙老师帅吗？！个么大BOSS是谁？！";
+
+
+  var text3 = function (m) { var r = 0; while (m > 1) r++, m >>= 1; return r; }
+  var type    = won ? "game-won" : "game-over";
+  var message = won ? "恭喜你！223个kiss from咩咩！！" : mytxt[text3(maxscore)-2];
+
+  if (typeof ga !== "undefined") {
+    ga("send", "event", "game", "end", type, this.score);
+  }
+
+  this.messageContainer.classList.add(type);
+  this.messageContainer.getElementsByTagName("p")[0].textContent = message;
+
+  this.clearContainer(this.sharingContainer);
+  this.sharingContainer.appendChild(this.scoreTweetButton());
+  twttr.widgets.load();
+};
+
+HTMLActuator.prototype.clearMessage = function () {
+  // IE only takes one value to remove at a time.
+  this.messageContainer.classList.remove("game-won");
+  this.messageContainer.classList.remove("game-over");
+};
+
+HTMLActuator.prototype.scoreTweetButton = function () {
+  var tweet = document.createElement("a");
+  tweet.classList.add("twitter-share-button");
+  tweet.setAttribute("href", "https://twitter.com/share");
+  tweet.setAttribute("data-via", "aenonsun");
+  tweet.setAttribute("data-url", "http://aenon.github.io/Kame");
+  tweet.setAttribute("data-counturl", "http://aenon.github.io/Kame");
+  tweet.textContent = "Tweet";
+
+  var text = "I scored " + this.score + " points at life, a game where you " +
+             "join ranks of biological classifications to score high! #Kame_class_game";
+  tweet.setAttribute("data-text", text);
+
+  return tweet;
+};
